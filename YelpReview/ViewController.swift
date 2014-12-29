042a21a0fc64ba0332aadd99c2c0a0b183883314
Michael Makarov <info1117@gmail.com>
2014-12-28 19:13:22 -0500

    Adds http

--- YelpReview/ViewController.swift
+++ YelpReview/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
 
+    @IBOutlet weak var businessPicker: UIPickerView!
+    @IBOutlet weak var businessLabel: UILabel!
+
+    let pickerData = ["Bar", "Hotel", "DMV"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        businessPicker.dataSource = self
+        businessPicker.delegate = self
     }
 
+    @IBAction func generate(sender: AnyObject) {
+        let view = self.storyboard?.instantiateViewControllerWithIdentifier("result") as resultViewController
+        view.generate(5, business: "bar")
+        self.navigationController?.pushViewController(view, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    //MARK: Delegates
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return pickerData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        businessLabel.text = pickerData[row]
+    }
+    
 }
 
---
+++ YelpReview/resultViewController.swift
@@ -0,0 +1,53 @@
+//
+//  resultViewController.swift
+//  YelpReview
+//
+//  Created by Michael on 12/28/14.
+//  Copyright (c) 2014 Michael. All rights reserved.
+//
+
+import UIKit
+
+class resultViewController: UIViewController {
+    
+    func getRating(stars: Int, business: String, completion: (rating: String) -> Void) {
+        var url = NSURL(string: "http://www.yelpreviewgenerator.com/review/\(stars)/\(business).json")
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                var error: NSError?;
+                let options = NSJSONReadingOptions(0)
+                let jsonObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!, options: options, error: &error)
+                
+                // Call closure
+                if let decodedObject = jsonObject as? NSDictionary {
+                    let review = decodedObject["review"]! as String
+                    completion(rating: review)
+                }
+            })
+        })
+        task.resume()
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func generate(stars: Int, business: String) -> Void {
+        getRating(4, business: "Bar") { (review) -> Void in
+            let alert = UIAlertView()
+            alert.title = "Hey"
+            alert.message = review
+            alert.addButtonWithTitle("Working!!")
+            alert.show()
+        }
+    
+    }
+
+}
